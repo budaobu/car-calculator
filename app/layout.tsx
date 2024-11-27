cda52abbabf3b2b48f402bca7ce1ffbbdba3a06a
bufang <aiguai131458+github@gmail.com>
2024-11-27 13:45:44 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,13 @@ export const metadata: Metadata = {
     description: '通过年行驶里程、充电条件、使用场景等多个维度，科学计算是否适合购买电动汽车或燃油车。',
     type: 'website',
     locale: 'zh_CN',
+    image: 'https://img.dakaiai.app/file/AgACAgEAAyEGAASJ9vGyAAMNZ0axvMaVItDWKif-H1CU1fyLKBoAAtCtMRsQJzlG99pOFtbsTQMBAAMCAAN5AAM2BA.png',
   },
   twitter: {
     card: 'summary_large_image',
     title: '买电车还是油车计算器 - 帮您做出明智的选择',
     description: '通过年行驶里程、充电条件、使用场景等多个维度，科学计算是否适合购买电动汽车或燃油车。',
+    image: 'https://img.dakaiai.app/file/AgACAgEAAyEGAASJ9vGyAAMNZ0axvMaVItDWKif-H1CU1fyLKBoAAtCtMRsQJzlG99pOFtbsTQMBAAMCAAN5AAM2BA.png',
   },
 }
 

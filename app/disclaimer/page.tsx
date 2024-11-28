b2d54de774b6069c5cb1a8f8fcf41b4275950f50
bufang <aiguai131458+github@gmail.com>
2024-11-28 21:26:31 +0800

    Update page.tsx

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -17,10 +17,16 @@ export default function DisclaimerPage() {
             </p>
             <h2 className="text-xl font-semibold mb-2">隐私声明</h2>
             <p className="mb-4">
-              我们重视您的隐私。本网站不收集、存储或处理任何个人识别信息。所有计算都在您的浏览器中本地进行，不会传输到我们的服务器。
+              我们重视您的隐私。本网站使用 Google Analytics 来收集匿名的使用数据，以帮助我们改进用户体验。这些数据不包含任何可以识别个人身份的信息。
+            </p>
+            <p className="mb-4">
+              我们还使用 Google 站长平台来优化网站在搜索引擎中的表现。这可能涉及到收集有关网站访问的一些技术数据。
+            </p>
+            <p className="mb-4">
+              所有的计算和核心功能都在您的浏览器中本地进行，不会传输到我们的服务器。我们不会收集、存储或处理任何您在计算器中输入的个人信息。
             </p>
             <p>
-              我们使用匿名的分析工具来了解网站的使用情况，以改进用户体验。这些工具不会收集任何可以识别个人身份的信息。
+              如果您希望了解更多关于 Google Analytics 的数据收集和使用政策，请访问 <a href="https://policies.google.com/technologies/partner-sites" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Google 隐私权和条款</a>。
             </p>
           </CardContent>
         </Card>
